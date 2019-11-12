6351c12a425b6f579b26d2ac01a31509b48e5def
zhenwang12 <zhen.wang12@outlook.com>
2019-11-11 22:38:28 -0800

    Update 266. Palindrome Permutation.java

--- 266. Palindrome Permutation.java
+++ 266. Palindrome Permutation.java
@@ -1,17 +1,22 @@
-Q: Given a string, determine if a permutation of the string could form a palindrome.
-
-A: 
-  public boolean canPermutePalindrome(String s) {
-          int[] table = new int[128];
-
-          for (char c: s.toCharArray()) {
-              int v = (int)c;
-              table[v]++;
-          }
-          int oddCount = 0;
-          for (int i : table) {
-              if (i%2 == 1)
-                  oddCount++;
-          }
-          return oddCount <= 1;
-      }
+/* Q: Given a string, determine if a permutation of the string could form a palindrome. 
+*
+*/
+    public boolean canPermutePalindrome(String s) {
+        //Assuming input is an ASCII string
+        int[] table = new int[128];
+        
+        //Count how many times each character appears
+        for (char c : s.toCharArray()) {
+            int v = (int) c;
+            table[v]++;
+        }
+        
+        //To form a palindrome, string can have at most one charater that is odd.
+        int oddCount = 0;
+        for (int i : table) {
+            if (i % 2 == 1)
+                oddCount++;
+        }
+     
+        return oddCount <= 1;
+    }
